86c18a1f1f949ec88476d3442d2f6279bb13f052
Elliot Mayer <elliotm@lclark.edu>
2025-02-27 22:17:41 -0800

    initial commit

--- test/test_tictactoe.py
+++ test/test_tictactoe.py
@@ -5,7 +5,7 @@ def test_finds_successor():
     assert successor(INITIAL_STATE, 1, 'X') == '.X.......'
 
 def test_finds_legal_moves():
-    assert legal_moves('.X...0...') == [0, 2, 3, 4, 6, 7 ,8]
+    assert legal_moves('.X...O...') == [0, 2, 3, 4, 6, 7, 8]
 
 def test_finds_win_for_x():
     assert winner('...XXX.OO') == 1
@@ -14,15 +14,22 @@ def test_finds_win_for_o():
     assert winner('...XX.OOO') == -1
 
 def test_finds_value_at_end_of_game():
-    assert min_value('...XXX.OO') == 1
-    assert max_value('...XXX.OO') == 1
+    assert value('...XXX.OO', 'O', less, 2) == 1
+    assert value('...XXX.OO', 'X', greater, -2) == 1
 
 def test_finds_win_in_one_move():
-    assert max_value('XX....O.O') == 1
+    assert value('XX....O.O', 'X', greater, -2) == 1
 
 def test_finds_value_after_two_moves():
-    assert min_value('XOXXXOO..') == 0
+    assert value('XOXXXOO..', 'O', less, 2) == 0
 
 def test_finds_value_after_three_moves():
-    assert min_value('.X.X.O.O.') == -1
-    assert max_value('.X.X.O.O.') == 1
+    assert value('.X.X.O.O.', 'X', greater, -2) == 1
+    assert value('.X.X.O.O.', 'O', less, 2) == -1
+
+def test_finds_best_move_depth_1():
+    assert best_move_for_x('XX.O....O')
+
+def test_finds_best_move_deep():
+    assert best_move_for_x('X..OO...X') == 5
+
